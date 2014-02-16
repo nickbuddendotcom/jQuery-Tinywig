9faf211e84cf896b82979398eb4dab9c2e83b031
nickbuddendotcom <hello@nickbudden.com>
2014-02-16 17:30:40 +0800

    basic boilerplate

--- gulpfile.js
+++ gulpfile.js
@@ -10,13 +10,22 @@ var gulp    = require('gulp'),
     concat  = require('gulp-concat'),
     uglify  = require('gulp-uglify'),
     rename  = require('gulp-rename'),
-    mocha   = require('gulp-mocha');
+    mocha   = require('gulp-mocha'),
+    prefix  = require('gulp-autoprefixer');
 
 // Lint Task
 gulp.task('lint', function() {
   gulp.src('./src/jquery.tinywig.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
+});
+
+// SASS
+gulp.task('sass', function() {
+    return gulp.src('./src/scss/*.scss')
+        .pipe(sass())
+        .pipe(prefix('last 1 version', '> 1%', 'ie 8', 'ie 7'))
+        .pipe(gulp.dest('./dist/css/'));
 });
 
 // Concatenate & Minify JS
@@ -36,7 +45,7 @@ gulp.task('test', function () {
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch('./src/jquery.tinywig.js', ['lint', 'scripts']);
+    gulp.watch('./src/jquery.tinywig.js', ['lint', 'scripts', 'sass']);
 });
 
 // Default Task
